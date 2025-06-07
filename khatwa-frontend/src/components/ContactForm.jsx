5a0651f39f2c5a0cab880996778de75276d269c2
Samira <samiirahassanii11@gmail.com>
2025-06-07 02:39:20 +0100

    version3  authentification , verification , oublier mot de pass

--- khatwa-frontend/src/components/ContactForm.jsx
+++ khatwa-frontend/src/components/ContactForm.jsx
@@ -25,23 +25,23 @@ export default function ContactSection() {
       <h2>Envie de nous parler ?</h2>
       <p>Remplissez le formulaire et notre équipe vous répondra sous 24h.</p>
     </div>
-     <form >           
-            <div className="name-inputs">
+     <form className="contact">           
+            <div className="nameInputs">
               <input type="text" name="prenom" placeholder="Prénom"  required />
               <input type="text" name="nom" placeholder="Nom"  required />
             </div>
-            <div className="age-country-inputs">
+            <div className="infoInputs">
               <input type="text" name="text" placeholder="objectif" required />
               <input type="email" name="email" placeholder="email" required />
 
             </div>
-            <div class="input-group">
+            <div class="group">
                   <label for="message">Message</label>
                   <textarea id="message" placeholder="Décrivez votre demande..."></textarea>
                 </div>         
               <motion.button
         type="submit"
-        class="btn-submit"
+        class="btn"
         variants={buttonHover}
         whileHover="scale"
       >
